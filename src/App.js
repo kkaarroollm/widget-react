c5ad38e6669c94c2133afe96d9158de4b1d1f05b
karol marszalek <mkarol.4514@gmail.com>
2023-05-12 21:11:06 +0200

    variables in config.js file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './index.css';
 import Countdown from "./countdown";
 import InfoDataCoin from "./raisedsold";
 import PriceDisplay from "./coin-api";
+import {priceApiUrl, targetDate, infoDataApiUrl} from "./config";
 
 function App() {
-    const targetDate = new Date("2023-05-20").getTime();
   return (
       <div>
-        <PriceDisplay apiUrl={'https://api.coindesk.com/v1/bpi/currentprice.json'}/>
-        <InfoDataCoin apiUrl={'https://api.coindesk.com/v1/bpi/currentprice.json'}/>
+        <PriceDisplay apiUrl={priceApiUrl}/>
+        <InfoDataCoin apiUrl={infoDataApiUrl}/>
         <Countdown targetDate={targetDate}/>
         <button id="connect-wallet">Connect wallet</button>
       </div>
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const priceApiUrl = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+export const infoDataApiUrl = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+export const targetDate = new Date("2023-05-20").getTime();
